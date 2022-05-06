39c8f3ca07828efc2797de415a6513c42fabcab2
paul <paulbrowne2@gmail.com>
2022-05-06 19:34:38 +0100

    Function for creating new post added to contract

---
+++ contracts/Blog.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.0
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Blog {
+    string public name;
+    address public owner;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _postIds;
+
+    struct Post {
+        uint id;
+        string title;
+        string content;
+        bool published;
+    }
+
+    mapping(uint => Post) private idToPost;
+    mapping(string => Post) private hashToPost;
+
+    event PostCreated(uint id, string title, string hash);
+    event PostUpdated(uint id, string title, string hash, bool published);
+
+    constructor(string memory _name) {
+        console.log("Deploying Blog with name:", _name);
+        name = _name:
+        owner = msg.sender;
+    }
+
+    function updateName(string memory _name) public {
+        name = _name;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    function fetchPost(string memory hash) public view returns(Post memory) {
+        return hashToPost[hash];
+    }
+
+    function createPost(string memory title, string memory hash) public onlyOwner {
+        _postIds.increment();
+        uint postId = _postIds.current();
+        Post storage post = idToPost[postId];
+        post.id = postId;
+        post.title = title;
+        post.published = true;
+        post.content = hash;
+        hashToPost[hash] = post;
+        emit PostCreated(postId, title, hash);
+    }
+}
